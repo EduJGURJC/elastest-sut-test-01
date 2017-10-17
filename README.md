a805f8e09e5b2a242259d494d73d6ccd454464a8
EduJGURJC <ejg.urjc@gmail.com>
2017-10-17 10:59:06 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test Results Path
+```
+/elastest-sut-test-01/target/surefire-reports/TEST-org.elastest.demo_test.DemoTest.xml
+```
---
+++ src/test/java/org/elastest/demo_test/DemoTest.java
@@ -0,0 +1,46 @@
+package org.elastest.demo_test;
+
+import org.junit.Test;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
+
+/**
+ * Simple example of plain Selenium usage.
+ */
+public class DemoTest {
+
+	@Test
+	public void simplePlainSeleniumTest() {
+
+		String appIP = System.getenv("APP_IP");
+//		String appIP = "http://172.27.0.11:8080";
+		System.out.println("SuT ip: " + appIP);
+
+		boolean works = validUrl(appIP);
+		System.out.println("assert");
+		assertTrue("The SuT is started and works fine", works);
+		// exitVnc(p);
+	}
+
+	public static boolean validUrl(String URLName) {
+		try {
+			HttpURLConnection.setFollowRedirects(false);
+			HttpURLConnection con = (HttpURLConnection) new URL(URLName).openConnection();
+			con.setRequestMethod("HEAD");
+			return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public void sleep(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
